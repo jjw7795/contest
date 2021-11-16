5e31a96c22714609e48d3d3e0d0f278e313d2b82
jjw7795 <jjw7795@naver.com>
2021-11-16 23:56:00 +0900

    alert 추가

--- public/js/apply.js
+++ public/js/apply.js
@@ -18,80 +18,83 @@ let register = document.querySelector(".register");
 //동의함
 $("#agree").click(function () {
   $("#agree").prop("checked", true);
+});
 
+$(".register").click(function () {
   if ($("#agree").is(":checked") == true) {
-    $(".register").click(function () {
-      //유저정보
-      let email = $("#email").val();
-      let pw = $("#password").val();
-      let name = $("#name").val();
-      let birth = $("#birth").val();
-      let address = $("#address").val();
-      let detailAddress = $("#detailAddress").val();
-      let phone = $("#phone").val();
-      let fileValue = $("#file").val().split("\\");
-      let fileName = fileValue[fileValue.length - 1];
-      console.log("ddd", fileName);
+    //유저정보
+    let email = $("#email").val();
+    let pw = $("#password").val();
+    let name = $("#name").val();
+    let birth = $("#birth").val();
+    let address = $("#address").val();
+    let detailAddress = $("#detailAddress").val();
+    let phone = $("#phone").val();
+    let fileValue = $("#file").val().split("\\");
+    let fileName = fileValue[fileValue.length - 1];
+    console.log("ddd", fileName);
 
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, pw)
-        .then((result) => {
-          let 유저정보 = {
-            name,
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pw)
+      .then((result) => {
+        let 유저정보 = {
+          name,
+          email,
+          birth,
+          address,
+          detailAddress,
+          phone,
+        };
+        db.collection("user").doc(result.user.uid).set(유저정보);
+
+        console.log("유저");
+        console.log(result);
+        console.log(result.user);
+        result.user.updateProfile({ displayName: name });
+      });
+
+    let file = document.querySelector("#file").files[0];
+    let storageRef = storage.ref();
+    let 저장할경로 = storageRef.child("file/" + file.name);
+    let 업로드작업 = 저장할경로.put(file);
+
+    업로드작업.on(
+      "state_changed",
+      null,
+      (error) => {
+        console.error("실패사유는", error);
+      },
+      () => {
+        업로드작업.snapshot.ref.getDownloadURL().then((url) => {
+          console.log("업로드된 경로는", url);
+
+          var 저장할거 = {
             email,
+            name,
+            date: new Date().toLocaleString(),
+            file: url,
+            fileName,
             birth,
             address,
             detailAddress,
             phone,
+            //   uid: JSON.parse(localStorage.getItem("user")).uid,
+            //   name: JSON.parse(localStorage.getItem("user")).displayName,
           };
-          db.collection("user").doc(result.user.uid).set(유저정보);
-
-          console.log("유저");
-          console.log(result);
-          console.log(result.user);
-          result.user.updateProfile({ displayName: name });
+          db.collection("apply")
+            .add(저장할거)
+            .then((result) => {
+              alert("접수가 성공적으로 되었습니다");
+              window.location.href = "/";
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         });
-
-      let file = document.querySelector("#file").files[0];
-      let storageRef = storage.ref();
-      let 저장할경로 = storageRef.child("file/" + file.name);
-      let 업로드작업 = 저장할경로.put(file);
-
-      업로드작업.on(
-        "state_changed",
-        null,
-        (error) => {
-          console.error("실패사유는", error);
-        },
-        () => {
-          업로드작업.snapshot.ref.getDownloadURL().then((url) => {
-            console.log("업로드된 경로는", url);
-
-            var 저장할거 = {
-              email,
-              name,
-              date: new Date().toLocaleString(),
-              file: url,
-              fileName,
-              birth,
-              address,
-              detailAddress,
-              phone,
-              //   uid: JSON.parse(localStorage.getItem("user")).uid,
-              //   name: JSON.parse(localStorage.getItem("user")).displayName,
-            };
-            db.collection("apply")
-              .add(저장할거)
-              .then((result) => {
-                window.location.href = "/";
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          });
-        }
-      );
-    });
+      }
+    );
+  } else {
+    alert("동의함 체크해주세요");
   }
 });
